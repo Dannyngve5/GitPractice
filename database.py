25b8f3b9f13406866ced86ebed50f4c953557344
Dannyngve5 <deparragz@gmail.com>
2026-07-10 14:43:50 -0500

    Change return function

--- database.py
+++ database.py
@@ -3,7 +3,11 @@ class Database:
         print("Connecting to database...")
 
     def disconnect(self):
+<<<<<<< HEAD
         return False
+=======
+        return True
+>>>>>>> bb478b6 (Change return function)
 
     def connect_alternative(self):
         """Initialize database connection."""
